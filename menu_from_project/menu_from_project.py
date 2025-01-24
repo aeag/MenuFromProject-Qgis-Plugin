f0c7bcd0f1fca53176c3be65bde574a68b0058fd
jmkerloch <jean-marie.kerloch@oslandia.com>
2025-01-24 15:48:13 +0100

    wip(settings): add settings widget to QGIS option

--- menu_from_project/menu_from_project.py
+++ menu_from_project/menu_from_project.py
@@ -49,6 +49,7 @@ from menu_from_project.logic.qgs_manager import (
 )
 from menu_from_project.logic.tools import icon_per_layer_type
 from menu_from_project.toolbelt.preferences import PlgOptionsManager
+from menu_from_project.ui.dlg_settings import PlgOptionsFactory
 from menu_from_project.ui.menu_conf_dlg import MenuConfDialog  # noqa: F4 I001
 from menu_from_project.ui.menu_layer_data_item_provider import MenuLayerProvider
 
@@ -58,10 +59,6 @@ from menu_from_project.ui.menu_layer_data_item_provider import MenuLayerProvider
 
 
 class MenuFromProject:
-
-    def on_initializationCompleted(self):
-        # build menu
-        self.initMenus()
 
     def __init__(self, iface):
         self.task = None
@@ -81,6 +78,8 @@ class MenuFromProject:
 
         self.iface = iface
         self.toolBar = None
+
+        self.options_factory = None
 
         self.qgs_dom_manager = QgsDomManager()
         self.menubarActions = []
@@ -365,6 +364,23 @@ class MenuFromProject:
     def initGui(self):
         settings = self.plg_settings.get_plg_settings()
         if settings.is_setup_visible:
+            # settings page within the QGIS preferences menu
+            if not self.options_factory:
+                self.options_factory = PlgOptionsFactory()
+                self.options_factory.settingsApplied.connect(self._apply_settings)
+                self.iface.registerOptionsWidgetFactory(self.options_factory)
+
+                # Add search path for plugin
+                help_search_paths = QgsSettings().value("help/helpSearchPath")
+                if (
+                    isinstance(help_search_paths, list)
+                    and __uri_homepage__ not in help_search_paths
+                ):
+                    help_search_paths.append(__uri_homepage__)
+                else:
+                    help_search_paths = [help_search_paths, __uri_homepage__]
+                QgsSettings().setValue("help/helpSearchPath", help_search_paths)
+
             # menu item - Main
             self.action_project_configuration = QAction(
                 QIcon(str(DIR_PLUGIN_ROOT / "resources/menu_from_project.png")),
@@ -392,7 +408,10 @@ class MenuFromProject:
                 partial(QDesktopServices.openUrl, QUrl(__uri_homepage__))
             )
 
-        self.iface.initializationCompleted.connect(self.on_initializationCompleted)
+        self.iface.initializationCompleted.connect(self._apply_settings)
+
+    def _apply_settings(self) -> None:
+        self.initMenus()
 
     def unload(self):
         menuBar = self.iface.editMenu().parentWidget()
@@ -410,6 +429,18 @@ class MenuFromProject:
 
         settings = self.plg_settings.get_plg_settings()
         if settings.is_setup_visible:
+            # -- Clean up preferences panel in QGIS settings
+            if self.options_factory:
+                self.iface.unregisterOptionsWidgetFactory(self.options_factory)
+                # pop from help path
+                help_search_paths = QgsSettings().value("help/helpSearchPath")
+                if (
+                    isinstance(help_search_paths, list)
+                    and __uri_homepage__ in help_search_paths
+                ):
+                    help_search_paths.remove(__uri_homepage__)
+                QgsSettings().setValue("help/helpSearchPath", help_search_paths)
+
             self.iface.removePluginMenu(
                 "&" + __title__, self.action_project_configuration
             )
@@ -419,14 +450,14 @@ class MenuFromProject:
             )
             self.iface.removePluginMenu(__title__, self.action_menu_help)
 
-        self.iface.initializationCompleted.disconnect(self.on_initializationCompleted)
+        self.iface.initializationCompleted.disconnect(self._apply_settings)
 
         if self.provider:
             self.registry.removeProvider(self.provider)
 
     def open_projects_config(self):
         dlg = MenuConfDialog(self.iface.mainWindow())
-        dlg.setModal(True)
+        dlg.setModal(False)
 
         dlg.show()
         result = dlg.exec()
---
+++ menu_from_project/ui/dlg_settings.py
@@ -0,0 +1,304 @@
+#! python3  # noqa: E265
+
+"""
+    Plugin settings dialog.
+"""
+
+import uuid
+from functools import partial
+
+# Standard library
+from pathlib import Path
+
+# PyQGIS
+from qgis.core import QgsApplication, QgsMessageLog
+from qgis.gui import QgsOptionsPageWidget, QgsOptionsWidgetFactory
+from qgis.PyQt import QtCore, uic
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction, QHeaderView, QMenu
+
+# project
+from menu_from_project.__about__ import (
+    DIR_PLUGIN_ROOT,
+    __title__,
+    __uri_homepage__,
+    __version__,
+)
+from menu_from_project.datamodel.project import Project
+from menu_from_project.toolbelt.preferences import (
+    SOURCE_MD_LAYER,
+    SOURCE_MD_NOTE,
+    SOURCE_MD_OGC,
+    PlgOptionsManager,
+)
+from menu_from_project.ui.project_list_model import ProjectListModel
+
+# ############################################################################
+# ########## Globals ###############
+# ##################################
+
+FORM_CLASS, FORM_BASE = uic.loadUiType(
+    Path(__file__).parent / "{}.ui".format(Path(__file__).stem)
+)
+
+# ############################################################################
+# ########## Classes ###############
+# ##################################
+
+
+class ConfigOptionsPage(FORM_CLASS, QgsOptionsPageWidget):
+    """Settings form embedded into QGIS 'options' menu."""
+
+    settingsApplied = QtCore.pyqtSignal()
+
+    def __init__(self, parent):
+        """Constructor."""
+        super().__init__(parent)
+        self.plg_settings = PlgOptionsManager()
+        self.setupUi(self)
+        self.setObjectName("mOptionsPage{}".format(__title__))
+
+        self.defaultcursor = self.cursor
+        self.setWindowTitle(
+            self.windowTitle() + " - {} v{}".format(__title__, __version__)
+        )
+        self.setWindowIcon(
+            QIcon(str(DIR_PLUGIN_ROOT / "resources/gear.svg")),
+        )
+
+        settings = self.plg_settings.get_plg_settings()
+        self.btnDelete.clicked.connect(self.onDelete)
+        self.btnDelete.setText(None)
+        self.btnDelete.setIcon(
+            QIcon(QgsApplication.iconPath("mActionDeleteSelected.svg"))
+        )
+        self.btnUp.clicked.connect(self.onMoveUp)
+        self.btnUp.setText(None)
+        self.btnUp.setIcon(QIcon(QgsApplication.iconPath("mActionArrowUp.svg")))
+        self.btnDown.clicked.connect(self.onMoveDown)
+        self.btnDown.setText(None)
+        self.btnDown.setIcon(QIcon(QgsApplication.iconPath("mActionArrowDown.svg")))
+
+        # add button
+        self.btnAdd.setIcon(QIcon(QgsApplication.iconPath("mActionAdd.svg")))
+        self.addMenu = QMenu(self.btnAdd)
+        add_option_file = QAction(
+            QIcon(QgsApplication.iconPath("mIconFile.svg")),
+            self.tr("Add from file"),
+            self.addMenu,
+        )
+        add_option_pgdb = QAction(
+            QIcon(QgsApplication.iconPath("mIconPostgis.svg")),
+            self.tr("Add from PostgreSQL database"),
+            self.addMenu,
+        )
+        add_option_http = QAction(
+            QIcon(str(DIR_PLUGIN_ROOT / "resources/globe.svg")),
+            self.tr("Add from URL"),
+            self.addMenu,
+        )
+        add_option_file.triggered.connect(partial(self.onAdd, "file"))
+        add_option_http.triggered.connect(partial(self.onAdd, "http"))
+        add_option_pgdb.triggered.connect(partial(self.onAdd, "database"))
+        self.addMenu.addAction(add_option_file)
+        self.addMenu.addAction(add_option_pgdb)
+        self.addMenu.addAction(add_option_http)
+        self.btnAdd.setMenu(self.addMenu)
+
+        # -- Options
+        self.cbxLoadAll.setChecked(settings.optionLoadAll)
+        self.cbxLoadAll.setTristate(False)
+
+        self.cbxCreateGroup.setChecked(settings.optionCreateGroup)
+        self.cbxCreateGroup.setTristate(False)
+
+        self.cbxShowTooltip.setChecked(settings.optionTooltip)
+        self.cbxShowTooltip.setTristate(False)
+
+        self.cbxOpenLinks.setChecked(settings.optionOpenLinks)
+        self.cbxOpenLinks.setTristate(False)
+
+        self.sourcesMdText = {
+            SOURCE_MD_OGC: self.tr("QGIS Server metadata"),
+            SOURCE_MD_LAYER: self.tr("Layer Metadata"),
+            SOURCE_MD_NOTE: self.tr("Layer Notes"),
+        }
+        self.optionSourceMD = settings.optionSourceMD
+
+        self.projetListModel = ProjectListModel(self)
+        self.projetListModel.set_project_list(settings.projects)
+        self.projectTableView.setModel(self.projetListModel)
+
+        self.projectTableView.selectionModel().selectionChanged.connect(
+            self._selected_project_changed
+        )
+        self.projectTableView.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeToContents
+        )
+
+        self.projectWidget.projectChanged.connect(self._project_changed)
+        if len(settings.projects) != 0:
+            self.projectTableView.selectRow(0)
+
+    def _selected_project_changed(self) -> None:
+        """Update displayed project"""
+        selected_index = self.projectTableView.selectionModel().selectedRows()
+        if selected_index:
+            row = selected_index[0].row()
+            project = self.projetListModel.data(
+                self.projetListModel.index(row, ProjectListModel.NAME_COL),
+                Qt.UserRole,
+            )
+            self.projectWidget.set_project(project)
+            self.projectWidget.enable_merge_option(row != 0)
+
+    def setSourceMdText(self):
+        self.mdSource1.setText(self.sourcesMdText[self.optionSourceMD[0]])
+        self.mdSource2.setText(self.sourcesMdText[self.optionSourceMD[1]])
+        self.mdSource3.setText(self.sourcesMdText[self.optionSourceMD[2]])
+
+    def onAccepted(self):
+        print("onAccepted")
+        settings = self.plg_settings.get_plg_settings()
+        settings.projects = []
+        for project in self.projetListModel.get_project_list():
+            # Only get project with file defined
+            if project.file:
+                settings.projects.append(project)
+
+        settings.optionTooltip = self.cbxShowTooltip.isChecked()
+        settings.optionLoadAll = self.cbxLoadAll.isChecked()
+        settings.optionCreateGroup = self.cbxCreateGroup.isChecked()
+        settings.optionOpenLinks = self.cbxOpenLinks.isChecked()
+
+        settings.optionSourceMD = self.optionSourceMD
+
+        PlgOptionsManager().save_from_object(settings)
+
+        self.settingsApplied.emit()
+
+    def onAdd(self, qgs_type_storage: str = "file"):
+        """Add a new line to the table.
+
+        :param qgs_type_storage: project storage type, defaults to "file"
+        :type qgs_type_storage: str, optional
+        """
+        project = Project(
+            id=str(uuid.uuid4()),
+            name="",
+            location="new",
+            type_storage=qgs_type_storage,
+            file="",
+        )
+        self.projetListModel.insert_project(project)
+        self.projectTableView.selectRow(self.projetListModel.rowCount() - 1)
+        self.projectTableView.scrollToBottom()
+
+    @staticmethod
+    def log(message, application=__title__, indent=0):
+        indent_chars = " .. " * indent
+        QgsMessageLog.logMessage(
+            f"{indent_chars}{message}", application, notifyUser=True
+        )
+
+    def onDelete(self):
+        """Remove selected lines from the table."""
+        selected_index = self.projectTableView.selectionModel().selectedRows()
+        if len(selected_index) > 0:
+            r = selected_index[0].row()
+            self.projetListModel.removeRows(r, 1)
+
+    def onMoveUp(self):
+        """Move the selected lines upwards."""
+        selected_index = self.projectTableView.selectionModel().selectedRows()
+        if len(selected_index) > 0:
+            r = selected_index[0].row()
+            if r > 0:
+                project_a = self.projetListModel.get_row_project(r - 1)
+                project_b = self.projetListModel.get_row_project(r)
+
+                if project_b.location == "merge" and r == 1:
+                    project_b.location = "new"
+
+                self.projetListModel.set_row_project(r - 1, project_b)
+                self.projetListModel.set_row_project(r, project_a)
+                self.projectTableView.selectRow(r - 1)
+
+    def onMoveDown(self):
+        """Move the selected lines down."""
+        selected_index = self.projectTableView.selectionModel().selectedRows()
+        if len(selected_index) > 0:
+            r = selected_index[0].row()
+            nbRows = self.projetListModel.rowCount()
+            if r < nbRows - 1:
+                project_a = self.projetListModel.get_row_project(r)
+                project_b = self.projetListModel.get_row_project(r + 1)
+
+                if project_b.location == "merge" and r == 0:
+                    project_b.location = "new"
+
+                self.projetListModel.set_row_project(r, project_b)
+                self.projetListModel.set_row_project(r + 1, project_a)
+                self.projectTableView.selectRow(r + 1)
+
+    def on_mdSource2_released(self):
+        myorder = [1, 0, 2]
+        self.optionSourceMD = [self.optionSourceMD[i] for i in myorder]
+        self.setSourceMdText()
+
+    def on_mdSource3_released(self):
+        myorder = [2, 0, 1]
+        self.optionSourceMD = [self.optionSourceMD[i] for i in myorder]
+        self.setSourceMdText()
+
+    def _project_changed(self) -> None:
+        """Update model when displayed project has changed"""
+        selected_index = self.projectTableView.selectionModel().selectedRows()
+        if len(selected_index) > 0:
+            project = self.projectWidget.get_project()
+            self.projetListModel.set_row_project(selected_index[0].row(), project)
+
+    def apply(self):
+        """Called to permanently apply the settings shown in the options page (e.g. \
+        save them to QgsSettings objects). This is usually called when the options \
+        dialog is accepted."""
+        print("apply")
+        settings = self.plg_settings.get_plg_settings()
+        settings.projects = []
+        for project in self.projetListModel.get_project_list():
+            # Only get project with file defined
+            if project.file:
+                settings.projects.append(project)
+
+        settings.optionTooltip = self.cbxShowTooltip.isChecked()
+        settings.optionLoadAll = self.cbxLoadAll.isChecked()
+        settings.optionCreateGroup = self.cbxCreateGroup.isChecked()
+        settings.optionOpenLinks = self.cbxOpenLinks.isChecked()
+
+        settings.optionSourceMD = self.optionSourceMD
+
+        PlgOptionsManager().save_from_object(settings)
+        self.settingsApplied.emit()
+
+
+class PlgOptionsFactory(QgsOptionsWidgetFactory):
+    settingsApplied = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.conf_widget = None
+
+    def icon(self):
+        return QIcon(str(DIR_PLUGIN_ROOT / "resources/images/icon.svg"))
+
+    def createWidget(self, parent):
+        widget = ConfigOptionsPage(parent)
+        widget.settingsApplied.connect(self.settingsApplied.emit)
+        return widget
+
+    def title(self):
+        return __title__
+
+    def helpId(self) -> str:
+        return __uri_homepage__
