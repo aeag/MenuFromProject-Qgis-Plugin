95344897d4c057ca474bb2ba5d12fbf701617dc9
jmkerloch <jean-marie.kerloch@oslandia.com>
2025-02-06 17:03:53 +0100

    feat(config dialog): add help button

--- menu_from_project/ui/dlg_settings.py
+++ menu_from_project/ui/dlg_settings.py
@@ -7,11 +7,15 @@
 # Standard library
 
 # PyQGIS
+from functools import partial
+
 from qgis.PyQt import uic
+from qgis.PyQt.Qt import QUrl
+from qgis.PyQt.QtGui import QDesktopServices
 from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox
 
 # project
-from menu_from_project.__about__ import DIR_PLUGIN_ROOT
+from menu_from_project.__about__ import DIR_PLUGIN_ROOT, __uri_homepage__
 
 # ############################################################################
 # ########## Globals ###############
@@ -33,6 +37,13 @@ class MenuConfDialog(QDialog, FORM_CLASS):
 
         self.buttonBox.button(QDialogButtonBox.Apply).clicked.connect(
             self.wdg_config.apply
+        )
+
+        self.buttonBox.button(QDialogButtonBox.Help).clicked.connect(
+            partial(
+                QDesktopServices.openUrl,
+                QUrl(__uri_homepage__),
+            )
         )
 
     def accept(self):
